1b30e943bbc647df3b1df5a01daed8a950de7542
AlexRFarnes <alexrfarnes@gmail.com>
2025-07-01 05:51:53 +0800

    draw hanged man

--- hangman.py
+++ hangman.py
@@ -27,3 +27,118 @@ def _validate_input(player_input, guessed_letter):
         and player_input in ascii_lowercase
         and player_input not in guessed_letter
     )
+
+
+def join_guessed_letters(guessed_letters):
+    return " ".join(sorted(guessed_letters))
+
+
+def build_guessed_word(target_word, guessed_letters):
+    current_letters = []
+    for letter in target_word:
+        if letter in guessed_letters:
+            current_letters.append(letter)
+        else:
+            current_letters.append("_")
+    return " ".join(current_letters)
+
+
+def draw_hanged_man(wrong_guesses):
+    hanged_man = [
+        r"""
+  -----
+  |   |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+      |
+      |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+  |   |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+ ---  |
+/     |
+|     |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+ ---  |
+/   \ |
+|   | |
+      |
+-------
+""",
+    ]
+    console.print(hanged_man[wrong_guesses])
+
+
+def pretty_print(hanged_man):
+    pass
