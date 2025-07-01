8bbe1ec8706d7d18b545765abf0174be2929e0d0
AlexRFarnes <alexrfarnes@gmail.com>
2025-07-02 06:28:32 +0800

    complete the game loop

--- hangman.py
+++ hangman.py
@@ -5,6 +5,8 @@ from string import ascii_lowercase
 
 from rich.console import Console
 
+MAX_INCORRECT_GUESSES = 6
+
 console = Console()
 
 
@@ -29,17 +31,23 @@ def _validate_input(player_input, guessed_letter):
     )
 
 
-def join_guessed_letters(guessed_letters):
-    return " ".join(sorted(guessed_letters))
+def join_guessed_letters(target_word, guessed_letters):
+    styled_guessed_letters = []
+    for letter in sorted(guessed_letters):
+        if letter in target_word:
+            styled_guessed_letters.append(f"[blue on green]{letter}[/]")
+        else:
+            styled_guessed_letters.append(f"[red]{letter}[/]")
+    return " ".join(styled_guessed_letters)
 
 
 def build_guessed_word(target_word, guessed_letters):
     current_letters = []
     for letter in target_word:
         if letter in guessed_letters:
-            current_letters.append(letter)
+            current_letters.append(f"[green]{letter}[/]")
         else:
-            current_letters.append("_")
+            current_letters.append("[dim]_[/]")
     return " ".join(current_letters)
 
 
@@ -140,5 +148,50 @@ def draw_hanged_man(wrong_guesses):
     console.print(hanged_man[wrong_guesses])
 
 
-def pretty_print(hanged_man):
-    pass
+def game_over(wrong_guesses, target_word, guessed_letters):
+    if wrong_guesses == MAX_INCORRECT_GUESSES:
+        return True
+    if set(target_word) <= guessed_letters:
+        return True
+    return False
+
+
+def print_header(headline):
+    console.rule(f"[bold blue]:video_game: {headline} :video_game:[/]\n")
+
+
+if __name__ == "__main__":
+    # Initial setup
+    target_word = select_word()
+    guessed_letters = set()
+    guessed_word = build_guessed_word(target_word, guessed_letters)
+    wrong_guesses = 0
+    print_header("Welcome to Hangman!")
+
+    # Game loop
+    while not game_over(wrong_guesses, target_word, guessed_letters):
+        draw_hanged_man(wrong_guesses)
+        console.print(f"Your word is: {guessed_word}")
+        console.print(
+            f"Current guessed letters: {join_guessed_letters(target_word, guessed_letters)}\n"
+        )
+        player_guess = get_player_input(guessed_letters)
+        if player_guess in target_word:
+            console.print(":confetti_ball: [green]Great guess![/] :confetti_ball:")
+        else:
+            console.print(":boom: [red]Sorry, it's not there[/] :boom:")
+            wrong_guesses += 1
+        guessed_letters.add(player_guess)
+        guessed_word = build_guessed_word(target_word, guessed_letters)
+
+    # Game over
+    draw_hanged_man(wrong_guesses)
+    if wrong_guesses == MAX_INCORRECT_GUESSES:
+        console.print(
+            ":disappointed_face: [red]Sorry, you lost![/] :disappointed_face:"
+        )
+    else:
+        console.print(
+            ":trophy: [bold green]Congrats![/] [green]You did it![/] :trophy:"
+        )
+    console.print(f"Your word was: {target_word}")
